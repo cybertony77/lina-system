f841ab906effa90850a9e3b24b472a9cabe1e3a7
Antony Joseph <cybertony77>
2026-05-06 01:13:08 +0300

    commetjhg87987

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -11,13 +11,13 @@ export default function Document() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
         {/* Theme & App Settings */}
-        <meta name="theme-color" content="#011E46" />
+        <meta name="theme-color" content="#f2b3d0" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="Lina System ❤️" />
+        <meta name="apple-mobile-web-app-title" content="Lina's System ❤️" />
         <meta name="apple-touch-fullscreen" content="yes" />
-        <meta property="og:title" content="Lina System ❤️" />
+        <meta property="og:title" content="Lina's System ❤️" />
         <meta property="og:description" content="Lina's Medicine System. Made with ❤️ by Tony Joseph" />
         <meta property="og:image" content="/icons/apple-icon-180.png" />
         <meta property="og:type" content="website" />
